0335d1bc714f9bbac4dea50670cf76a4673686e2
Phoebe Hugh <phoebehugh@gmail.com>
2015-04-28 13:26:57 +0100

    Takes error function out to be a
    outside of the form function

--- new_directory_js/js/gitprofile.js
+++ new_directory_js/js/gitprofile.js
@@ -1,5 +1,9 @@
 $(document).ready(function() { // makes available and safe to be rendered
 
+    var displayError = function(errorMessage){ $('.container').prepend(errorMessage) };
+
+    var clearInput = function(){ $('input.username').val('') };
+
   $('.gitprofile').on('submit', function(e) { // when someone enters their name, it executes the function
 
     e.preventDefault(); //event.preventDefault
@@ -8,15 +12,10 @@ $(document).ready(function() { // makes available and safe to be rendered
 
     var template = $('template').html(); // just returns content of selected element in jquery
 
-    var displayError = function(){ $('.container').prepend("User not found") };
-
-    var clearInput = function(){ $('input.username').val('') };
-
-
     $.get(url, function(info) {
       $('.container').prepend(Mustache.render(template, info)); //prepend inserts the content at the top of the container
     }).fail(function() {
-      displayError();
+      displayError('User not found');
     }).always(function() { // always executed regardless of what happens
       clearInput();
       });
